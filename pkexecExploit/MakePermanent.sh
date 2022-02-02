43784a142735021c2768dd8944687c627e2502bb
Ankit_ojha <60637916+Ankit-Ojha16@users.noreply.github.com>
2022-02-02 14:55:36 +0530

    Add files via upload

---
+++ pkexecExploit/MakePermanent.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+cp pkexecExploit /usr/bin/
+chmod +x /usr/bin/pkexecExploit
+echo "Setting up the Privilages..."
+sleep 2
+clear
+echo ""
+echo "============================================"
+echo "Hey 5h4d0w Here..."  
+echo "CVE-2021-4034"
+echo "pxexec Local Privilege Escalation exploit"
+echo "============================================"
+echo ""
+
+echo "--------------------------------------------"
+echo "Type 'pkexecExploit' to run the Exploit"
+echo "--------------------------------------------"
+echo ""
+exit
+
